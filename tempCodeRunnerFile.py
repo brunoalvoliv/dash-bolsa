cfa5e68467f0cdb5030504dd96cc14c1b3370c23
Bruno Alves <brunoalvesdeoliveira96@gmail.com>
2022-02-23 22:04:15 -0300

    Update tempCodeRunnerFile.py

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,9 +1,1 @@
-import pandas as pd
-
-dataBase = []
-
-for i in range(1, 13):
-    cpas = pd.read_csv(f'/home/brunoalvoliver/Documentos/Programação/dash-bolsa/CPAs/CPA{i}.csv')
-    dataBase.append(cpas)
-
-print(dataBase[0])
+app.run_server(debug=True)
